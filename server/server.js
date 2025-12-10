2a7a4e0f84c5011dcb0c204ccbe9b522a58df51e
bikesh-khatri <khatribikesh366@gmail.com>
2025-12-10 19:04:12 +0545

    welcome screen for guest done

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
-const app = express();
-const PORT = 5001;
+const path = require('path');
 
-// Example route
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+// Serve static files from the frontend (client/public)
+app.use(express.static(path.join(__dirname, '../client/src')));
+
+// Example API route
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from server!' });
+});
+
+// Catch-all route to serve index.html for any other request
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.sendFile(path.join(__dirname, '../client/src/app.js'));
+});
+
+app.get('/rajan', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/public/rajan.html'));
 });
 
 app.listen(PORT, () => {
