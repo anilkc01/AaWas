968d54a3e24f13fbe9a38d0289e71bd48874de06
bikesh-khatri <khatribikesh366@gmail.com>
2025-12-17 18:22:52 +0545

    welcome screen for guest done

---
+++ client/src/AppContent.jsx
@@ -0,0 +1,35 @@
+import { Routes, Route, useLocation } from "react-router-dom";
+import Navbar from "./components/NavBar1";
+import Welcome from "./pages/welcome";
+import LoginModal from "./components/LoginModal";
+import RegisterModal from "./components/RegistrationModal";
+
+export default function AppContent() {
+  const location = useLocation();
+  const isLogin = location.pathname === "/login";
+  const isRegister = location.pathname === "/register";
+  const showModal = isLogin || isRegister;
+
+  return (
+    <>
+      <div className={showModal ? "filter blur-sm pointer-events-none" : ""}>
+        <Navbar />
+        <Welcome />
+      </div>
+      
+      {/*isLogin && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="absolute inset-0 bg-black/20"></div>
+          <LoginModal />
+        </div>
+      )}
+      
+      {isRegister && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="absolute inset-0 bg-black/20"></div>
+          <RegisterModal />
+        </div>
+      )*/}
+    </>
+  );
+}
